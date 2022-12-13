0ca4b4054fd317d9ba9b14083d6a2122847e6c82
rebecca <fulkerson.rebecca@gmail.com>
2022-12-13 11:30:50 -0500

    added middleware auth (strip/verify token) to book router

--- routes/BookRouter.js
+++ routes/BookRouter.js
@@ -1,10 +1,30 @@
 const Router = require('express').Router()
 const controller = require('../controllers/BookController')
+const middleware = require('../middleware')
 
-Router.post('/', controller.createBook)
+Router.post(
+    '/', 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.createBook
+)
+
 Router.get('/', controller.getAllBooks)
+
 Router.get('/:book_id', controller.getBookById)
-Router.put('/:book_id', controller.updateBook)
-Router.delete('/:book_id', controller.deleteBook)
+
+Router.put(
+    '/:book_id',
+    middleware.stripToken,
+    middleware.verifyToken, 
+    controller.updateBook
+)
+
+Router.delete(
+    '/:book_id', 
+    middleware.stripToken,
+    middleware.verifyToken,
+    controller.deleteBook
+)
 
 module.exports = Router 
