a099a98970c8316ea4d8121c819a658de5bc991d
rebecca <fulkerson.rebecca@gmail.com>
2022-12-13 01:04:48 -0500

    added controllers and routes for book reports; functional and tested succesfully in insomnia

---
+++ controllers/BookReportController.js
@@ -0,0 +1,63 @@
+const { BookReport } = require('../models')
+
+const createBookReport = async (req, res) => {
+    try {
+        const bookReport = await new BookReport(req.body)
+        await bookReport.save()
+        res.send(bookReport)
+    } catch (error) {
+        throw error
+    }
+}
+
+const getAllBookReports = async (req, res) => {
+    try {
+        const bookReports = await BookReport.findAll()
+        res.send(bookReports)
+    } catch (error) {
+        throw error
+    }
+}
+
+const getBookReportById = async (req, res) => {
+    try {
+        const { bookReport_id } = req.params
+        const bookReport = await BookReport.findByPk(bookReport_id)
+        res.send(bookReport)
+    } catch (error) {
+        throw error
+    }
+}
+
+const updateBookReport = async (req, res) => {
+    try {
+        const bookReport = await BookReport.update(
+            {...req.body},
+            { where: { id: req.params.bookReport_id }, returning: true}
+        )
+        res.send(bookReport)
+    } catch (error) {
+        throw error
+    }
+}
+
+const deleteBookReport = async (req, res) => {
+    try {
+        await BookReport.destroy({ where: { id: req.params.bookReport_id}})
+        res.send({
+            msg: 'Book report deleted',
+            payload: req.params.bookReport_id,
+            status: 'Ok'
+        })
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {
+    createBookReport,
+    getAllBookReports,
+    getBookReportById,
+    updateBookReport, 
+    deleteBookReport
+}
--- routes/BookReportRouter.js
+++ routes/BookReportRouter.js
@@ -1,3 +1,10 @@
 const Router = require('express').Router()
+const controller = require('../controllers/BookReportController')
+
+Router.post('/', controller.createBookReport)
+Router.get('/', controller.getAllBookReports)
+Router.get('/:bookReport_id', controller.getBookReportById)
+Router.put('/:bookReport_id', controller.updateBookReport)
+Router.delete('/:bookReport_id', controller.deleteBookReport)
 
 module.exports = Router 
